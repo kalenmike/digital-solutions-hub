d3588c82041d2d05e56cf9cc8e0cae37318abce5
Kalen Michael <kalenmike@gmail.com>
2026-02-11 16:24:24 +0100

    feat: add clients marquee

---
+++ app/components/index/Clients.vue
@@ -0,0 +1,43 @@
+<template>
+  <h2 class="text-2xl font-semibold text-center mb-20">
+    Trusted by <span class="text-primary">top brands</span> across industries
+  </h2>
+  <UMarquee pause-on-hover>
+    <ThemedImage
+      src="/images/logos/vluksmedia.svg"
+      alt="Vluks Media"
+      height="12"
+    />
+
+    <img src="/images/logos/ultralytics.svg" alt="Ultralytics" class="h-8" />
+    <img
+      src="/images/logos/lm-bridal-house.png"
+      alt="Bridal House"
+      class="h-16"
+    />
+
+    <ThemedImage
+      src="/images/logos/promediatics.png"
+      alt="Promediatics"
+      height="12"
+    />
+
+    <img src="/images/logos/lm-sickode.svg" alt="Sickode" class="h-16" />
+    <ThemedImage
+      src="/images/logos/hostel-consulting.png"
+      alt="Hostel.Consulting"
+      height="12"
+    />
+    <ThemedImage
+      src="/images/logos/provo-elite-charters.png"
+      alt="Provo Elite Charters"
+      height="16"
+    />
+    <ThemedImage
+      src="/images/logos/wolves-tours.png"
+      alt="Wolves Tours"
+      height="16"
+    />
+    <img src="/images/logos/fluence.png" alt="Fluence" class="h-6" />
+  </UMarquee>
+</template>
--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,9 +1,10 @@
 <template>
   <div>
-    <IndexLanding/>
-    <LazyIndexProcess/>
-    <LazyIndexAbout/>
+    <IndexLanding />
+    <LazyIndexProcess />
+    <LazyIndexAbout />
     <LazyIndexServices />
+    <LazyIndexClients />
     <LazyIndexCTA />
     <LazyIndexIndividuals />
     <LazyIndexDevelopers />
