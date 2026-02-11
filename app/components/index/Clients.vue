f7070feaa5ad202e2ed0d2cc4162246987511739
Kalen Michael <kalenmike@gmail.com>
2026-02-11 17:06:50 +0100

    chore: reorder logos

--- app/components/index/Clients.vue
+++ app/components/index/Clients.vue
@@ -8,26 +8,24 @@
       alt="Vluks Media"
       height="12"
     />
-
-    <img src="/images/logos/ultralytics.svg" alt="Ultralytics" class="h-8" />
-    <img
-      src="/images/logos/lm-bridal-house.png"
-      alt="Bridal House"
-      class="h-16"
-    />
-
     <ThemedImage
       src="/images/logos/promediatics.png"
       alt="Promediatics"
       height="12"
     />
-
+    <img
+      src="/images/logos/lm-bridal-house.png"
+      alt="Bridal House"
+      class="h-16"
+    />
+    <img src="/images/logos/ultralytics.svg" alt="Ultralytics" class="h-8" />
     <img src="/images/logos/lm-sickode.svg" alt="Sickode" class="h-16" />
     <ThemedImage
       src="/images/logos/hostel-consulting.png"
       alt="Hostel.Consulting"
       height="12"
     />
+    <ThemedImage src="/images/logos/fluence.png" alt="Fluence" height="6" />
     <ThemedImage
       src="/images/logos/provo-elite-charters.png"
       alt="Provo Elite Charters"
@@ -38,6 +36,5 @@
       alt="Wolves Tours"
       height="16"
     />
-    <ThemedImage src="/images/logos/fluence.png" alt="Fluence" height="6" />
   </UMarquee>
 </template>
