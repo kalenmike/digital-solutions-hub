2f2f9985d9da159cf92f2fd950763fc771815ad1
Kalen Michael <kalenmike@gmail.com>
2026-02-12 20:09:27 +0100

    feat: improve seo

--- app/app.vue
+++ app/app.vue
@@ -1,7 +1,50 @@
 <script setup lang="ts">
 import type { FooterColumn } from "@nuxt/ui";
+const route = useRoute();
+const config = useRuntimeConfig();
+const base = "https://kalenmichael.com";
 
-const title = "Digital Solutions Hub";
+// This computed property updates whenever the route changes
+const canonicalUrl = computed(
+  () => `${base}${route.path === "/" ? "" : route.path}`,
+);
+
+useHead({
+  link: [{ rel: "canonical", href: canonicalUrl }],
+  script: [
+    {
+      type: "application/ld+json",
+      children: JSON.stringify([
+        {
+          "@context": "https://schema.org",
+          "@type": "WebSite",
+          name: "Digital Solutions Hub",
+          url: "https://kalenmichael.com",
+        },
+        {
+          "@context": "https://schema.org",
+          "@type": "ProfessionalService",
+          name: "Digital Solutions Hub",
+          image: "https://kalenmichael.com/images/km-logo-grey.svg",
+          sameAs: [
+            "https://www.linkedin.com/in/kalenmichael/",
+            "https://github.com/kalenmike",
+          ],
+          "@id": "https://kalenmichael.com",
+          url: "https://kalenmichael.com",
+          knowsAbout: [
+            "Digital Transformation",
+            "SEO Consulting",
+            "Web Development",
+            "JavaScript SEO",
+          ],
+        },
+      ]),
+    },
+  ],
+});
+
+const title = "Digital Solutions Hub | Kalen Michael";
 const description =
   "Helping businesses and individuals bring their digital ideas to life";
 const siteUrl = "https://kalenmichael.com";
@@ -20,8 +63,8 @@ onMounted(() => {
   const contactUrl = `${window.location.origin}/#contact`;
   console.log(
     "%c 👋 Hello there! %c Looking for a developer? %c \n\nMessage me from this site: %c" +
-    contactUrl +
-    "%c\nOr email me directly: %c hello@kalenmichael.com",
+      contactUrl +
+      "%c\nOr email me directly: %c hello@kalenmichael.com",
     "color: white; background: #3b82f6; padding: 5px 10px; border-radius: 5px 0 0 5px; font-weight: bold;",
     "color: #3b82f6; background: #eff6ff; padding: 5px 10px; border-radius: 0 5px 5px 0; font-weight: bold;",
     "color: #64748b; font-style: italic;", // Career text
@@ -31,58 +74,41 @@ onMounted(() => {
   );
 });
 
-const columns: FooterColumn[] = [
-  {
-    label: "Quick Links",
-    children: [
-      {
-        label: "About",
-        to: "#about",
-      },
-      {
-        label: "Services",
-        to: "#services",
-      },
-      {
-        label: "How I Help",
-        to: "#support",
-      },
-      {
-        label: "For Developers",
-        to: "#developers",
-      },
-      {
-        label: "Contact",
-        to: "#contact",
-      },
-    ],
-  },
-];
-
 const navItems = [
   {
     label: "About",
-    to: "#about",
+    to: "/#about",
   },
   {
     label: "Services",
-    to: "#services",
+    to: "/#services",
   },
   {
     label: "How I Help",
-    to: "#support",
+    to: "/#support",
   },
   {
     label: "For Developers",
-    to: "#developers",
+    to: "/#developers",
   },
   {
     label: "Contact",
-    to: "#contact",
+    to: "/#contact",
   },
   {
     label: "Tools",
     to: "/external-tools",
+  },
+  {
+    label: "Blog",
+    to: "/blog",
+  },
+];
+
+const columns: FooterColumn[] = [
+  {
+    label: "Quick Links",
+    children: [...navItems],
   },
 ];
 
@@ -99,7 +125,8 @@ const showGrain = false;
           class="animate-noise absolute -inset-[10%] h-[120%] w-[120%] opacity-[0.05] mix-blend-soft-light dark:mix-blend-overlay"
           style="
             background-image: url(&quot;data:image/svg+xml,%3Csvg viewBox='0 0 200 200' xmlns='http://www.w3.org/2000/svg'%3E%3Cfilter id='n'%3E%3CfeTurbulence type='fractalNoise' baseFrequency='0.8' numOctaves='4' stitchTiles='stitch'/%3E%3C/filter%3E%3Crect width='100%25' height='100%25' filter='url(%23n)'/%3E%3C/svg%3E&quot;);
-          "></div>
+          "
+        ></div>
       </div>
     </div>
 
@@ -111,18 +138,35 @@ const showGrain = false;
       </template>
 
       <template #right>
-        <UNavigationMenu color="neutral" variant="link" :items="navItems" class="hidden lg:flex" />
+        <UNavigationMenu
+          color="neutral"
+          variant="link"
+          :items="navItems"
+          class="hidden lg:flex"
+        />
 
         <UColorModeButton />
 
-        <UButton to="https://github.com/kalenmike" target="_blank" icon="i-simple-icons-github" aria-label="GitHub"
-          color="neutral" variant="ghost" />
+        <UButton
+          to="https://github.com/kalenmike"
+          target="_blank"
+          icon="i-simple-icons-github"
+          aria-label="GitHub"
+          color="neutral"
+          variant="ghost"
+        />
       </template>
       <template #body>
-        <UNavigationMenu color="neutral" variant="link" :items="navItems" orientation="vertical" :ui="{
-          link: 'text-xl justify-center py-4',
-          item: 'w-full text-center',
-        }" />
+        <UNavigationMenu
+          color="neutral"
+          variant="link"
+          :items="navItems"
+          orientation="vertical"
+          :ui="{
+            link: 'text-xl justify-center py-4',
+            item: 'w-full text-center',
+          }"
+        />
       </template>
     </UHeader>
 
@@ -130,59 +174,80 @@ const showGrain = false;
       <NuxtPage />
     </UMain>
 
-    <USeparator icon="i-lucide-thumbs-up" />
+    <!-- <USeparator icon="i-lucide-thumbs-up" /> -->
 
-    <UFooter>
+    <UFooter class="bg-[var(--ui-bg)] -mt-2 relative shadow-2xl">
       <template #top>
         <UContainer>
-          <div class="flex flex-col md:flex-row justify-between items-start gap-20">
+          <div
+            class="flex flex-col md:flex-row justify-between items-start gap-20"
+          >
             <div class="w-80">
-              <h3 class="text-lg font-bold">Kalen Michael</h3>
-              <p class="mt-2 text-m text-gray-500 dark:text-gray-400 tracking-wide">
+              <p class="text-lg font-bold">Kalen Michael</p>
+              <p
+                class="mt-2 text-m text-gray-500 dark:text-gray-400 tracking-wide"
+              >
                 Digital Solutions Consultant helping businesses and individuals
                 bring their digital ideas to life.
               </p>
             </div>
 
             <div class="flex-1">
-              <UFooterColumns :columns="[
-                {
-                  label: 'Quick Links',
-                  children: [
-                    {
-                      label: 'About',
-                      to: '#about',
-                    },
-                    {
-                      label: 'Services',
-                      to: '#services',
-                    },
-                    {
-                      label: 'How I Help',
-                      to: '#support',
-                    },
-                    {
-                      label: 'For Developers',
-                      to: '#developers',
-                    },
-                    {
-                      label: 'Contact',
-                      to: '#contact',
-                    },
-                  ],
-                },
-              ]" :ui="{
+              <UFooterColumns
+                :columns="[
+                  {
+                    label: 'Quick Links',
+                    children: [
+                      {
+                        label: 'About',
+                        to: '#about',
+                      },
+                      {
+                        label: 'Services',
+                        to: '#services',
+                      },
+                      {
+                        label: 'How I Help',
+                        to: '#support',
+                      },
+                      {
+                        label: 'For Developers',
+                        to: '#developers',
+                      },
+                      {
+                        label: 'Contact',
+                        to: '#contact',
+                      },
+                    ],
+                  },
+                ]"
+                :ui="{
                   label: 'mb-3 text-sm',
                   list: 'space-y-1 mt-0',
                   base: 'py-0',
-                }" />
+                }"
+              />
             </div>
             <div class="flex-1">
-              <h3 class="font-semibold mb-3 text-sm">Connect</h3>
-              <UButton to="https://github.com/kalenmike" target="_blank" icon="i-simple-icons-github"
-                aria-label="GitHub" color="neutral" variant="ghost" size="xl" />
-              <UButton to="https://www.linkedin.com/in/kalenmichael/" target="_blank" icon="i-simple-icons-linkedin"
-                aria-label="GitHub" color="neutral" variant="ghost" size="xl" />
+              <p class="font-semibold mb-3 text-sm">Connect</p>
+              <UButton
+                to="https://github.com/kalenmike"
+                target="_blank"
+                icon="i-simple-icons-github"
+                aria-label="GitHub"
+                color="neutral"
+                variant="ghost"
+                size="xl"
+              />
+              <UButton
+                to="https://www.linkedin.com/in/kalenmichael/"
+                target="_blank"
+                icon="i-simple-icons-linkedin"
+                aria-label="GitHub"
+                color="neutral"
+                variant="ghost"
+                size="xl"
+              />
             </div>
           </div>
         </UContainer>
